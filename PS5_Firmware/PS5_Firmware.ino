0834d31174e8acfbfe62b0bac76ec9a69899a55f
gerardo <gcerpaalcantar@apple.com>
2024-03-16 13:18:52 -0700

    changed name and modified PS5 firmware

--- PS5_Firmware/PS5_Firmware.ino
+++ PS5_Firmware/PS5_Firmware.ino
@@ -7,7 +7,7 @@ HardwareSerial stm32Serial(1);
 void setup() {
   stm32Serial.begin(9600, SERIAL_8N1, rxPin, txPin);
   Serial.begin(9600);
-  ps5.begin("24:a6:fa:c7:f9:2d"); //replace with MAC address of your controller
+  ps5.begin("88:03:4c:ef:be:54"); //replace with MAC address of your controller
   Serial.println("Ready.");
 }
 
@@ -23,13 +23,19 @@ void loop() {
     int Cross = ps5.Cross() ? 0 : 1;
     int Circle = ps5.Circle() ? 0 : 1;
     int Square = ps5.Square() ? 0 : 1;
-    int Triangle = ps5.Triangle() ? 0: 1;
+    int Triangle = ps5.Triangle() ? 0 : 1;
     int PSButton = ps5.PSButton() ? 0 : 1;
 
     // Format the data string
-    char dataString[50];
-    
-    snprintf(dataString, sizeof(dataString), "%d %d %d %d %d %d %d %d", R2Value, L2Value, LStickXValue, Cross, Circle, Square, Triangle, PSButton);
+    char dataString[30];
+
+    int formattedLength = snprintf(dataString, sizeof(dataString), "%d:%d:%d:%d:%d:%d:%d:%d", R2Value, L2Value, LStickXValue, Cross, Circle, Square, Triangle, PSButton);
+
+    // Fill remaining characters with ':'
+    for (int i = formattedLength; i < sizeof(dataString) - 1; i++) {
+      dataString[i] = ':';
+    }
+    dataString[sizeof(dataString) - 1] = '\0'; // Null-terminate the string
 
     // Send the data string over serial
     stm32Serial.println(dataString);
@@ -37,6 +43,6 @@ void loop() {
     Serial.println(dataString);
     // This delay is to make the output more human readable
     // Remove it when you're not trying to see the output
+    delay(100);
   }
-  delay(100);
 }
